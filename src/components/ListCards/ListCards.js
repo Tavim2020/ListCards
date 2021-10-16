96c3422e6df760424c14c3169cd09b63d887f5a8
Tavim2020 <tavimvidaloka@gmail.com>
2021-10-16 14:16:45 -0300

    first commit

---
+++ src/components/ListCards/ListCards.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './style/ListCards.css';
+
+const ListCards = () => {
+    return (
+        <section className="listCards">
+            <nav>
+                <ul>
+                    <li>God Of War</li>
+                </ul>
+            </nav>
+
+            <nav>
+                <ul>
+                    <li>Devil My Cry</li>
+                </ul>
+            </nav>
+
+            <nav>
+                <ul>
+                    <li>FIFA 2021</li>
+                </ul>
+            </nav>
+
+            <nav>
+                <ul>
+                    <li>Lineage II</li>
+                </ul>
+            </nav>
+            
+        </section>
+    )
+}
+
+export default ListCards;
